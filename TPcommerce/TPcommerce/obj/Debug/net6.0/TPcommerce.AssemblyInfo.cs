39fa3cb47db9b952623c2ec80cbf3476abc718d5
Simon Drolet <simondrolet29@gmail.com>
2025-04-01 13:21:19 -0400

    tweak

--- TPcommerce/TPcommerce/obj/Debug/net6.0/TPcommerce.AssemblyInfo.cs
+++ TPcommerce/TPcommerce/obj/Debug/net6.0/TPcommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b87f847dc21e1e6f5bbb8919c3eae2bddd92a82d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c27bb4ef5a1e6d4e4c61b202b0871a166b0157")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
